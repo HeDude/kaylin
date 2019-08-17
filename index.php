d81b72005f38a95874bcf5d1ee72214026f81d9d
bijbrengen <slagter@bijbrengen.nl>
2019-08-17 17:55:47 +0200

    Started website

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE HTML>
+<html lang="nl">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<title>Kaylin Slagter - Filmcreator</title>
+
+	<link href="css/main.css" rel="stylesheet"/>
+</head>
+
+<body class="index preload">
+	<div id="page">
+		<header id="header">
+			<h1 id="logo"><a href="#"><img id="logo_image" src="image/logo.svg" alt="logo">&nbsp;&nbsp;Kaylin <span>Slagter</span></a></h1>
+			<nav id="nav">
+				<ul>
+					<li class="current"><a href="#">Welkom</a></li>
+					<li class="menu"><a href="#filmcreator">Filmcreator</a></li>
+                    <li class="menu"><a href="#talent">Talent</a></li>
+					<li class="menu"><a href="#storyteller">Verhalenverteller</a></li>
+					<li class="menu"><a href="#contact" class="button primary">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		<section id="banner">
+			<div id="filmcreator" class="inner">
+				<header>
+					<h2>Filmcreator</h2>
+				</header>
+				<p>Welkom op mijn website.<br/>Mijn naam is <strong>Kaylin</strong> Slagter, beginnend Filmcreator,<br/>en ik heb een grote passie voor het<br/>ontwikkelen, bedenken, ontwerpen, schrijven, creëren
+					<br/>van <strong>fantasie films</strong>.<br/>
+					Zie ook mijn <a href="https://www.youtube.com/channel/UCWlRxN9kmlyB5F-tnqoW0-g" target="_blank">YouTube kanaal</a>.
+				</p>
+				<footer>
+					<ul class="buttons stacked">
+						<li><a href="https://www.youtube.com/channel/UCWlRxN9kmlyB5F-tnqoW0-g" target="_blank" class="button fit scrolly">&#9658; Play</a></li>
+					</ul>
+				</footer>
+			</div>
+		</section>
+
+		<article id="main">
+			<h2>&nbsp;</h2>
+			<section id="talent" class="wrapper style1 container special">
+				<h2>Talent</h2>
+				<div class="row">
+					<div class="col-3 col-12-narrower">
+
+						<section>
+							<span class="icon solid fa-check"></span>
+							<header>
+								<h3>Creatief</h3>
+							</header>
+							<p>Creativiteit is voor mij heel belangrijk. Ik gebruik het als een veilige manier om mijn emoties te kunnen uiten. Film maken en schrijven is als een uitlaatklep waarbij ik dit met anderen kan delen.</p>
+						</section>
+
+					</div>
+					<div class="col-3 col-12-narrower">
+
+						<section>
+							<span class="icon solid fa-check"></span>
+							<header>
+								<h3>Taalvaardig</h3>
+							</header>
+							<p>Mijn gevoel voor taal is van jongs af aan aanwezig geweest en snel ontwikkeld door het kijken van vele films en series, waar ook mijn passie voor het film maken vandaan komt.</p>
+						</section>
+
+					</div>
+					<div class="col-3 col-12-narrower">
+
+						<section>
+							<span class="icon solid fa-check"></span>
+							<header>
+								<h3>Perfectionistisch</h3>
+							</header>
+							<p>In mijn leven heb ik vaak geworsteld met mijn perfectionisme. Het hield me tegen om hetgeen te doen wat ik leuk vind. Gelukkig heb ik inmiddels geleerd dit te omarmen en de mooie kanten ervan te kunnen zien.</p>
+						</section>
+
+					</div>
+					<div class="col-3 col-12-narrower">
+
+						<section>
+							<span class="icon solid fa-check"></span>
+							<header>
+								<h3>Ondernemend</h3>
+							</header>
+							<p>Het ontdekken van ondernemen was voor mij een hele openbaring. Ik heb geleerd om het heft in eigen handen te nemen en onafhankelijkheid op te bouwen. Ideaal voor het produceren van mijn eigen films.</p>
+						</section>
+
+					</div>
+				</div>
+			</section>
+
+    		<section id="storyteller" class="wrapper style3 container special">
+
+				<header class="major">
+					<h2>Verhalenverteller</h2>
+				</header>
+
+				<div class="row">
+					<div class="col-6 col-12-narrower">
+
+						<section>
+							<a href="#" class="image featured"><img src="image/verhalen.jpg" alt="" /></a>
+							<header>
+								<h3>Verhalen</h3>
+							</header>
+							<p>Ik heb veel verhalen geschreven waarin ik probeer de lezer mee te nemen in een fantasie wereld of een spannend verhaal, om mensen een mooie ervaring te bezorgen, vanuit hun luie stoel.</p>
+						</section>
+
+					</div>
+					<div class="col-6 col-12-narrower">
+
+						<section>
+							<a href="#" class="image featured"><img src="image/levenslessen.jpg" alt="" /></a>
+							<header>
+								<h3>Levenslessen</h3>
+							</header>
+							<p>Niet alleen de mooie kanten van het leven komen in mijn verhalen aan bod, maar ook situaties waarin het hoofdpersonage zaken tegenkomt, waaruit hij/zij en de lezer een waardevolle levensles leert.</p>
+						</section>
+
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-6 col-12-narrower">
+
+						<section>
+							<a href="#" class="image featured"><img src="image/reflectie.jpg" alt="" /></a>
+							<header>
+								<h3>Reflectie</h3>
+							</header>
+							<p>Het zijn herkenbare situaties, waaruit men troost of begrip kan halen voor zijn/haar eigen problemen. Vanaf een veilige afstand, kan men naar zichzelf kijken en meedenken over een mogelijke oplossing.</p>
+						</section>
+
+					</div>
+					<div class="col-6 col-12-narrower">
+
+						<section>
+							<a href="#" class="image featured"><img src="image/fantasie.jpg" alt="" /></a>
+							<header>
+								<h3>Fantasie</h3>
+							</header>
+							<p>Door fantasieverhalen, gecombineerd met de realiteit, creër ik situaties die voor iedereen herkenbaar zijn. Zo hoop ik mensen mee te kunnen nemen en een boodschap over te brengen.</p>
+						</section>
+
+					</div>
+				</div>
+
+				<footer class="major">
+					<ul class="buttons">
+						<li><a href="#" class="button">Kijk meer</a></li>
+					</ul>
+				</footer>
+
+			</section>
+
+		</article>
+
+		<section id="contact">
+
+			<header>
+				<h2>Met mij een <strong>film</strong> maken?</h2>
+				<p>Neem dan contact met me op!</p>
+			</header>
+			<footer>
+				<ul class="buttons">
+					<li><a href="mailto:kaylin@carinte.nl" class="button primary">Mail me</a></li>
+					<li><a href="https://wa.me/31636555235?text=Ik%20zou%20graag%20eens%20afspreken" class="button">App me</a></li>
+				</ul>
+			</footer>
+
+		</section>
+
+		<footer id="footer">
+			<ul class="icons">
+				<li><a href="https://www.youtube.com/channel/UCWlRxN9kmlyB5F-tnqoW0-g" target="_blank" class="icon brands circle fa-youtube"><span class="label">YouTube</span></a></li>
+				<li><a href="https://www.instagram.com/kaylinslagter/" target="_blank" class="icon brands circle fa-instagram"><span class="label">YouTube</span></a></li>
+			</ul>
+			<ul class="copyright">
+				<li>&copy; Kaylin Slagter</li><li>Design: <a href="http://hedude.com" target="_blank">HeDude</a></li>
+			</ul>
+		</footer>
+
+	</div>
+</body>
+</html>
